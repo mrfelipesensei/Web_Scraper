f1f7f4fc847bac059eda74e48fb72d5b4ff9c989
Felipe Greg <mrfelipesensei@gmail.com>
2025-03-14 15:06:29 -0300

    buscar_noticias()

--- index.py
+++ index.py
@@ -1,5 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
+import csv
 
 #URL da página do G1
 URL = "https://g1.globo.com/"
@@ -35,3 +36,25 @@ def coletar_noticias():
         )
 
     return noticias
+
+
+def salvar_csv(noticias):
+    with open("noticias_g1.csv","w",newline="",encoding="utf-8") as arquivo:
+        escritor = csv.DictWriter(arquivo, fieldnames=["titulo","link","palavras_chave"])
+        escritor.writeheader()
+        escritor.writerows(noticias)
+
+
+def buscar_noticias(noticias,palavra):
+    resultados = [n for n in noticias if palavra.lower() in  n["titulo"].lower() or palavra.lower() in n["palavras_chave"].lower()]
+
+    if not resultados:
+        print(f"Nenhuma notíficia encontrada com '{palavra}'.")
+    else:
+        print(f"\n Notícias encontradas para '{palavra}':\n")
+        for i, noticia in enumerate(resultados,start=1):
+            print(f"{i}. {noticia['titulo']}")
+            print(f"{noticia['link']}")
+            print(f"Palavras-chave: {noticia['palavras_chave']}\n")
+
+
